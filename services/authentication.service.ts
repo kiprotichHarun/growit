6355ed8a890900df8f2f59eb66290486a0a60179
kiprotichHarun <34366433+kiprotichHarun@users.noreply.github.com>
2019-08-28 10:41:57 +0300

    Add files via upload

---
+++ services/authentication.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor() { }
+}
