ecd2c3bc2e7186ea681a1f29558245fefc2b3340
kiprotichHarun <34366433+kiprotichHarun@users.noreply.github.com>
2019-10-06 21:58:43 +0300

    Add files via upload

--- mfarm/mfarm.page.ts
+++ mfarm/mfarm.page.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { File } from "@ionic-native/file/ngx";
+import {FileTransfer,FileTransferObject} from "@ionic-native/file-transfer/ngx";
+import { FileOpener } from "@ionic-native/file-opener/ngx";
+import {DocumentViewer} from '@ionic-native/document-viewer/ngx'
 
-import { ActionSheetController } from '@ionic/angular';
 import { SMS } from '@ionic-native/sms/ngx';
 import { AlertController } from '@ionic/angular';
+
 
 
 
@@ -13,11 +17,18 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./mfarm.page.scss'],
 })
 export class MfarmPage implements OnInit {
-  
+  fileTransfer: FileTransferObject;
   browser: any
   constructor( private router:Router,
     private sms: SMS,
-    public alertController: AlertController) { }
+    public alertController: AlertController,
+    private fileOpener: FileOpener,
+    private file: File,
+    private ft: FileTransfer,
+    private document: DocumentViewer
+   ) { }
+
+    
   go(){
     this.router.navigateByUrl('/home')
   }
@@ -25,7 +36,26 @@ export class MfarmPage implements OnInit {
   ngOnInit() {
   }
   
- 
-}
+  download(){
+    let doownloadUrl ='https://firebasestorage.googleapis.com/v0/b/growit-1d290.appspot.com/o/angularjs_tutorial.pdf?alt=media&token=60b6644e-ade7-4409-be44-4e091d048c4c';
+    let path = this.file.dataDirectory;
+    const transfer = this.ft.create();
+    
+    transfer.download(doownloadUrl,`${path}myfile.pdf`).then(entry =>{
+      let url = entry.toURL();
+      this.fileOpener.open(url, 'application/pdf');
+    });
 
+  }
+  dairycare(){
+    let doownloadUrl ='https://firebasestorage.googleapis.com/v0/b/growit-1d290.appspot.com/o/dairy%20care.pdf?alt=media&token=46bea3ab-03e0-4731-9a1f-14e4d91dfdfb';
+    let path = this.file.dataDirectory;
+    const transfer = this.ft.create();
+    
+    transfer.download(doownloadUrl,`${path}myfile.pdf`).then(entry =>{
+      let url = entry.toURL();
+      this.fileOpener.open(url, 'application/pdf');
+    })
+  }
+}
 
