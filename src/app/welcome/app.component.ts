eb1b82a2b3f4a9343c192f27b0a4a2032a527545
kiprotichHarun <34366433+kiprotichHarun@users.noreply.github.com>
2019-10-06 22:01:33 +0300

    Add files via upload

--- src/app/welcome/app.component.ts
+++ src/app/welcome/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -13,11 +12,13 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
+    
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
+    
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
--- src/app/welcome/app.module.ts
+++ src/app/welcome/app.module.ts
@@ -11,15 +11,29 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 //this is the camera plugin
 import { Camera } from '@ionic-native/camera/ngx';
+// file opening
+import {File} from '@ionic-native/file/ngx';
+import{FileOpener} from '@ionic-native/file-opener/ngx';
+import {FileTransfer} from '@ionic-native/file-transfer/ngx'
+import {DocumentViewer} from '@ionic-native/document-viewer/ngx'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import{Contacts} from '@ionic-native/contacts/ngx';
+import{CallNumber} from '@ionic-native/call-number/ngx'
+
+import {HttpModule} from '@angular/http'
 //grants us access to basic firebase functionality
+import {AngularFirestoreModule} from '@angular/fire/firestore';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth'; //helps with authentication in firebase
 import { environment } from '../environments/environment';
+import * as firebase from 'firebase/app';
+import { UserService } from './services/user.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 //import firebaseConfig from './firebase'
-//firebase.initializeApp(environment.firebase);
+firebase.initializeApp(environment.firebase);
 
 @NgModule({
   declarations: [AppComponent],
@@ -29,7 +43,12 @@ import { environment } from '../environments/environment';
     AutosizeModule,
      AppRoutingModule,
      IonicStorageModule.forRoot(),
+     AngularFirestoreModule,
+     ReactiveFormsModule,
+     FormsModule,
+     
     AngularFireModule.initializeApp(environment.firebase,'GROWIT'),
+    HttpModule,
   AngularFireAuthModule],
 
   providers: [
@@ -37,6 +56,14 @@ import { environment } from '../environments/environment';
     SMS,
     SplashScreen,
     Camera,
+    Contacts,
+    CallNumber,
+    UserService,
+    FileOpener,
+    File,
+    FileTransfer,
+   DocumentViewer,
+    
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
