66fdf9e27bc463f019ac2634c541f4a2a97b6d86
kiprotichHarun <34366433+kiprotichHarun@users.noreply.github.com>
2019-10-13 13:25:21 +0300

    Add files via upload

--- esoko/esoko.page.ts
+++ esoko/esoko.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {Router} from '@angular/router';
 //import { NavController } from '@ionic/angular';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
 //storage module
 import { Storage } from '@ionic/storage';
 import { SMS } from '@ionic-native/sms/ngx';
@@ -30,11 +30,11 @@ export class EsokoPage implements OnInit {
   imageURL:string
   desc: string
   //child of the button file
-  @ViewChild('fileButton') fileButton
+  @ViewChild('fileButton',{static: false }) fileButton
   // define the “currentImage” variable and inject the Camera into this class via the constructor:
   currentImage: any;
 
-  constructor(private sms: SMS ,private storage: Storage, private camera: Camera, 
+  constructor(private sms: SMS ,private storage: Storage, 
     private router:Router, public http:Http, public afstore: AngularFirestore, public user: UserService, private afs: AngularFirestore ) {
       
       const posts = afs.doc(`users/cEBynmz9ClXj66doTUaX`)
@@ -42,21 +42,7 @@ export class EsokoPage implements OnInit {
      }
 
  
-  takePicture() {
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
 
-    this.camera.getPicture(options).then((imageData) => {
-      this.currentImage = 'data:image/jpeg;base64,' + imageData;
-    }, (err) => {
-     // Handle error
-     console.log("Camera issue:" + err);
-    });
-  }
 
   go(){
     this.router.navigateByUrl('/home')
