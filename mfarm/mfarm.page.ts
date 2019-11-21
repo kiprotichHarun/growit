ede84f73e5c510062a83501183b8c0403356b738
kiprotichHarun <34366433+kiprotichHarun@users.noreply.github.com>
2019-11-21 16:54:44 +0300

    Add files via upload

--- mfarm/mfarm.page.ts
+++ mfarm/mfarm.page.ts
@@ -7,7 +7,8 @@ import {DocumentViewer} from '@ionic-native/document-viewer/ngx'
 import {Http} from '@angular/http';
 import { SMS } from '@ionic-native/sms/ngx';
 import { AlertController, } from '@ionic/angular';
-import { firestore } from 'firebase/app';
+import { VideoPlayer } from '@ionic-native/video-player/ngx';
+import { StreamingMedia, StreamingVideoOptions } from '@ionic-native/streaming-media/ngx';
 
 @Component({
   selector: 'app-mfarm',
@@ -26,6 +27,8 @@ export class MfarmPage implements OnInit {
     private file: File,
     private ft: FileTransfer,
     private document: DocumentViewer,
+    // private videoPlayer: VideoPlayer,
+    private streamingMedia: StreamingMedia
 
    ) { }
 
@@ -37,19 +40,8 @@ export class MfarmPage implements OnInit {
   ngOnInit() {
   }
   
-  download(){
-    let doownloadUrl ='https://firebasestorage.googleapis.com/v0/b/growit-1d290.appspot.com/o/angularjs_tutorial.pdf?alt=media&token=60b6644e-ade7-4409-be44-4e091d048c4c';
-    let path = this.file.dataDirectory;
-    const transfer = this.ft.create();
-    
-    transfer.download(doownloadUrl,`${path}myfile.pdf`).then(entry =>{
-      let url = entry.toURL();
-      this.fileOpener.open(url, 'application/pdf');
-    });
-
-  }
   dairycare(){
-    let doownloadUrl ='https://firebasestorage.googleapis.com/v0/b/growit-1d290.appspot.com/o/dairy%20care.pdf?alt=media&token=46bea3ab-03e0-4731-9a1f-14e4d91dfdfb';
+    let doownloadUrl ='https://firebasestorage.googleapis.com/v0/b/growit-1d290.appspot.com/o/the-life-of-dairy-cows.pdf?alt=media&token=8c543584-c48b-4cc0-943c-9b2c92abe407';
     let path = this.file.dataDirectory;
     const transfer = this.ft.create();
     
@@ -58,6 +50,99 @@ export class MfarmPage implements OnInit {
       this.fileOpener.open(url, 'application/pdf');
     })
   }
+//pdf for cattle diseases
+  dairydisase(){
+    let doownloadUrl ='https://firebasestorage.googleapis.com/v0/b/growit-1d290.appspot.com/o/Diseases%20of%20Dairy%20Cows.ppt?alt=media&token=40850ad8-d038-41f8-b4d6-664d47d740de';
+    let path = this.file.dataDirectory;
+    const transfer = this.ft.create();
+    
+    transfer.download(doownloadUrl,`${path}mypdf.pdf`).then(entry =>{
+      let url = entry.toURL();
+      this.fileOpener.open(url, 'application/pdf');
+    })
+  }
+// for dairy business
+  dairybusiness(){
+    let doownloadUrl ='https://firebasestorage.googleapis.com/v0/b/growit-1d290.appspot.com/o/naivasha.pdf?alt=media&token=2fa37ec8-5478-4940-9d03-4e6f282bee3b';
+    let path = this.file.dataDirectory;
+    const transfer = this.ft.create();
+    
+    transfer.download(doownloadUrl,`${path}mybook.pdf`).then(entry =>{
+      let url = entry.toURL();
+      this.fileOpener.open(url, 'application/pdf');
+    })
+  }
+// tomato growing
+Tomamtogr(){
+  let doownloadUrl ='https://firebasestorage.googleapis.com/v0/b/growit-1d290.appspot.com/o/Crop%20Guide_%20Tomato1.pdf?alt=media&token=4c8e3333-c163-498a-a29d-b88633373672';
+  let path = this.file.dataDirectory;
+  const transfer = this.ft.create();
+  
+  transfer.download(doownloadUrl,`${path}mykitabu.pdf`).then(entry =>{
+    let url = entry.toURL();
+    this.fileOpener.open(url, 'application/pdf');
+  })
+} 
+// tomamto care
+tomatocare(){
+  let doownloadUrl ='https://firebasestorage.googleapis.com/v0/b/growit-1d290.appspot.com/o/Crop%20Guide_%20How%20to%20Grow%20%26%20Growing%20Tomato2.pdf?alt=media&token=29abbb58-7834-48e8-8d54-18119644898a';
+  let path = this.file.dataDirectory;
+  const transfer = this.ft.create();
+  
+  transfer.download(doownloadUrl,`${path}myfile.pdf`).then(entry =>{
+    let url = entry.toURL();
+    this.fileOpener.open(url, 'application/pdf');
+  })
+} 
 
+// potato care
+
+potato(){
+  let doownloadUrl ='https://firebasestorage.googleapis.com/v0/b/growit-1d290.appspot.com/o/Crops%20Extension%20Pocket%20Hand%20Book-1%20(1).pdf?alt=media&token=df2f429f-9907-4f41-88c2-23b247df728a';
+  let path = this.file.dataDirectory;
+  const transfer = this.ft.create();
+  
+  transfer.download(doownloadUrl,`${path}viazi.pdf`).then(entry =>{
+    let url = entry.toURL();
+    this.fileOpener.open(url, 'application/pdf');
+  })
 }
 
+// maize farming
+
+maize(){
+  let doownloadUrl ='https://firebasestorage.googleapis.com/v0/b/growit-1d290.appspot.com/o/Crops%20Extension%20Pocket%20Hand%20Book-1%20(1).pdf?alt=media&token=df2f429f-9907-4f41-88c2-23b247df728a';
+  let path = this.file.dataDirectory;
+  const transfer = this.ft.create();
+  
+  transfer.download(doownloadUrl,`${path}mahindi.pdf`).then(entry =>{
+    let url = entry.toURL();
+    this.fileOpener.open(url, 'application/pdf');
+  })
+}
+
+// fruits
+fruits(){
+  let doownloadUrl ='https://firebasestorage.googleapis.com/v0/b/growit-1d290.appspot.com/o/Crops%20Extension%20Pocket%20Hand%20Book-1%20(1).pdf?alt=media&token=df2f429f-9907-4f41-88c2-23b247df728a';
+  let path = this.file.dataDirectory;
+  const transfer = this.ft.create();
+  
+  transfer.download(doownloadUrl,`${path}myfile.pdf`).then(entry =>{
+    let url = entry.toURL();
+    this.fileOpener.open(url, 'application/pdf');
+  })
+}
+
+playVideoLocal(){
+  let options: StreamingVideoOptions = {
+    successCallback: () => { console.log('Video played') },
+    errorCallback: (e) => { console.log('Error streaming') },
+    orientation: 'potrait',
+    shouldAutoClose: true,
+    controls: true
+  };
+  
+  this.streamingMedia.playVideo('https://firebasestorage.googleapis.com/v0/b/growit-1d290.appspot.com/o/(1)%20IONIC%20VIDEO%20PLAYER%20TUTORIAL%209%20-%20YouTube.mp4?alt=media&token=a4d00c12-7b93-4a63-b953-29f8251f702e', options);
+}
+
+}
